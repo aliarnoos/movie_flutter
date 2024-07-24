d7cbc18bbb3a649fbca8ed9092d8df855e19c749
aliarnoos <alimarnoos@gmail.com>
2024-07-25 01:37:35 +0300

    feat: add api service for http requests

---
+++ lib/services/api_service.dart
@@ -0,0 +1,37 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+
+class ApiService {
+  final String baseUrl = 'https://api.themoviedb.org/3/genre/movie/list';
+  final String apiKey = '166fcac7b52dc2e16a40ebfb58bec49b';
+
+  ApiService();
+
+  // Fetch a list of trending movies
+  Future<List<dynamic>> fetchTrendingMovies() async {
+    final response = await http.get(
+      Uri.parse(
+          '$baseUrl/discover/movie?api_key=$apiKey&sort_by=popularity.desc'),
+    );
+
+    if (response.statusCode == 200) {
+      Map<String, dynamic> data = json.decode(response.body);
+      return data['results'];
+    } else {
+      throw Exception('Failed to load movies');
+    }
+  }
+
+  // Fetch details of a specific movie by id
+  Future<dynamic> fetchMovieDetails(int movieId) async {
+    final response = await http.get(
+      Uri.parse('$baseUrl/movie/$movieId?api_key=$apiKey'),
+    );
+
+    if (response.statusCode == 200) {
+      return json.decode(response.body);
+    } else {
+      throw Exception('Failed to load movie details');
+    }
+  }
+}
