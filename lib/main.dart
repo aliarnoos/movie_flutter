a5c2734986eeb343b38546eacb94353a206dcf18
aliarnoos <alimarnoos@gmail.com>
2024-07-25 14:45:10 +0300

    build: fix android build with firebase

--- lib/main.dart
+++ lib/main.dart
@@ -8,6 +8,7 @@ import 'ui/screens/movie_screen.dart';
 import 'package:firebase_core/firebase_core.dart';
 
 void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
   runApp(const MyApp());
 }
